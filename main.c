d1a320263eb7fcccdd8190a8f6241f4a8d5b6bdc
Meqdad Amr <meqdad_@outlook.com>
2023-08-26 01:22:32 +0300

    Ch15: VM, Stack, Arithmetic operators.

--- main.c
+++ main.c
@@ -1,21 +1,37 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
+
+void write256Constant(Chunk *chunk){
+    for (int i = 0; i <= 256; ++i) {
+        writeConstant(chunk, 100+i, 123);
+    }
+}
 
 
 int main() {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
-    for (int i = 0; i <= 256; ++i) {
-        writeConstant(&chunk, 1.2, 123);
-    }
+    writeConstant(&chunk, 1.2, 123);
+    writeConstant(&chunk, 3.4, 123);
+    writeChunk(&chunk, OP_ADD, 123);
+
+    writeConstant(&chunk, 5.6, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
+
     writeChunk(&chunk, OP_RETURN, 123);
 
     printLines(&chunk.lines);
-    disassembleChunk(&chunk, "test chunk");
 
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
